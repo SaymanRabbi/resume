f737680210b93d1b47bc2868cb049559cdf617f7
saymanrabbi <mdrabbi.asm@gmail.com>
2023-05-27 22:20:16 +0600

    footer done

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,6 +1,7 @@
 import { faFacebookF, faInstagram, faLinkedinIn, faPinterestP, faYoutube } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import footerItem from './footerItem';
 FontAwesomeIcon.icon = {
     faFacebookF,
     faYoutube,
@@ -14,49 +15,64 @@ const Footer = () => {
             name: 'Facebook',
             link:'#',
             icon: faFacebookF,
-            color: '#3B5998'
+            color: 'bg-[#3B5998]'
         },
         {
             name: 'Youtube',
             link:'#',
             icon: faYoutube,
-            color: '#FFFFFF'
+            color: 'bg-[#FFFFFF]'
         },
         {
             name: 'Pinterest',
             link:'#',
             icon:faPinterestP,
-            color: '#CD1F28'
+            color: 'bg-[#CD1F28]'
         },
         {
             name: 'Instagram',
             link:'#',
             icon: faInstagram,
-            color: '#F08152'
+            color: 'bg-[#F08152]'
         },{
             name: 'Linkedin',
             link:'#',
             icon: faLinkedinIn,
-            color: '#0077B5'
+            color: 'bg-[#0077B5]'
         }
     ]
     return (
-        <div className='bg-[#0F141E] pb-[20px]'>
-            <div className='max-w-[1120px] mx-auto pt-[80px] pb-[30px]'>
-            <div className=' w-[100%]'>
+        <div className='bg-[#0F141E] pb-[20px] px-5'>
+            <div className='max-w-[1120px] mx-auto md:pt-[80px] pt-[40px] pb-[30px]'>
+            <div className=' w-[100%] md:flex '>
                <div className=' footer_socail w-[260px]'>
                    <h2 className=' text-2xl text-white font-[600]'>Connect with us on social media</h2>
                    <div className=' flex gap-x-2 mt-[10px]'>
                     {
                         social.map((item,index)=> (
-                            <div className={` w-[35px] h-[35px] flex justify-center items-center bg-gray-50 rounded-full bg-opacity-10  hover:bg-[${item.color}]`} key={index}>
+                            <div className={` w-[35px] h-[35px] flex justify-center items-center rounded-full bg-opacity-10  ${item.name==='Youtube' ? `${item?.color} text-[#CD1F28]` : `${item?.color} text-white`} hover:bg-opacity-100`} key={index}>
                               <FontAwesomeIcon icon={item.icon} className={`
-                              text-white text-xl cursor-pointer`}/>
+                               text-xl cursor-pointer`}/>
                           </div>
                         )
                         )
                     }
                    </div>
+               </div>
+               <div className=' flex flex-1 md:justify-end flex-wrap mt-[30px] md:mt-0'>
+                    {
+                     footerItem.map((item,index)=> (
+                        <div className=' min-w-[160px] mr-[48px] mb-[32px]' key={index}>
+                            <h2 className=' text-gray-600  font-[600]'>{item.title}</h2>
+                               {
+                                item.links.map((link,index)=> (
+                                    <a href={link.link} className=' block text-white text-[16px] mt-[10px] hover:text-blue-500 font-[500]' key={index}>{link.name}</a>
+                                ))
+                               }
+                            </div>
+                     ))
+                    }
+                
                </div>
             </div>
         </div>
---
+++ src/Components/Footer/footerItem.js
@@ -0,0 +1,89 @@
+const footerItem = [
+  {
+    title: "JOB SEEKERS",
+    links: [
+        {
+            name:'Create a CV',
+             link:'#'
+        },{
+            name:'CV Examples',
+            link:'#'
+        },{
+            name:'CV Templates',
+            link:'#'
+        },{
+            name:'Cover Letter Templates',
+            link:'#'
+        },{
+            name:'Job Search',
+            link:'#'
+        }
+    ]
+  },{
+    title: "CAREER RESOURCES",
+    links: [
+        {
+            name:'Resume Help',
+            link:'#'
+        },{
+            name:'Job Interview',
+            link:'#'
+        },{
+            name:'Career',
+            link:'#'
+        },{
+            name:'Cover Letter',
+            link:'#'
+        },{
+            name:'Blog',
+            link:'#'
+        }
+    ]
+  },{
+    title: "OUR COMPANY",
+    links: [
+        {
+            name:'About Us',
+            link:'#'
+        },{
+            name:'Pricing',
+            link:'#'
+        },{
+            name:'Product Updates',
+            link:'#'
+        },{
+            name:'Sponsorship Program',
+            link:'#'
+        },{
+            name:'Media Kit',
+            link:'#'
+        },{
+            name:'Affiliate Program',
+            link:'#'
+        }
+    ]
+  },{
+    title: "CONTACT & SUPPORT",
+    links: [
+        {
+            name:'FAQ',
+            link:'#'
+        },{
+            name:'Contact Us',
+            link:'#'
+        },{
+            name:'Terms and Conditions',
+            link:'#'
+        },{
+            name:'Privacy Policy',
+            link:'#'
+        },{
+            name:'Right of Withdrawal',
+            link:'#'
+        }
+    ]
+  }
+
+]
+
+export default footerItem;
